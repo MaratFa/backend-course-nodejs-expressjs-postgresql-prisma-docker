2ded59c960a2b0ccf9545c0f48aaa795d9df00f1
MaratFa <evolution99@rambler.ru>
2025-04-04 15:17:31 +0300

    Edited

--- server.js
+++ server.js
@@ -20,12 +20,21 @@ app.get("/", (req, res) => {
     <body style="background:pink; color: blue;">
       <h1>DATA:</h1>
         <p>${JSON.stringify(data)}</p>
+        <a href="/dashboard">Dashboard</a>
     </body>
     `);
 });
 
 app.get("/dashboard", (req, res) => {
-  res.send("<h1>dashboard</h1>");
+  res.send(`
+    <body>
+    <h1>dashboard</h1>
+    <a href="/">home</a>
+    </body>
+
+
+    
+    `);
 });
 
 // Type 2 - API endpoints (non visual)
@@ -44,6 +53,12 @@ app.post("/api/data", (req, res) => {
   console.log(newEntry);
   data.push(newEntry.name);
   res.sendStatus(201);
+});
+
+app.delete("/api/data", (req, res) => {
+  data.pop();
+  console.log("We deleted the element off the end of the array");
+  res.sendStatus(203);
 });
 
 app.listen(PORT, () => console.log(`Server has started on: ${PORT}`));
